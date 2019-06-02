cc7f26d51cd5830d0e88a5b2084592e62ec1ee22
n4rjk00 <chunwu@uw.edu>
2019-06-01 23:28:50 -0700

    ready to merge

--- gcmc/gcmc_model.py
+++ gcmc/gcmc_model.py
@@ -255,6 +255,167 @@ class GCMC(Model):
 
 
 
+class BlinearLogistic(Model):
+    def __init__(self, placeholders, params):
+        super().__init__(self, placeholders, params)
+        self.model_name = 'BilinearLogistic'
+
+    def build(self, placeholders, params):
+        # === pass model parameters ===
+        user_features_columns = params.user_features_columns
+        item_features_columns = params.item_features_columns
+
+        dim_user_raw = params.dim_user_raw
+        dim_item_raw = params.dim_item_raw
+        dim_user_conv = params.dim_user_conv
+        dim_item_conv = params.dim_item_conv
+        dim_user_embedding = params.dim_user_embedding
+        dim_item_embedding = params.dim_item_embedding
+
+        num_basis = params.num_basis
+        is_stacked = params.is_stacked
+        classes = params.classes
+        dropout = params.dropout
+        regularizer = tf.contrib.layers.l2_regularizer
+        regularizer_parameter = params.regularizer_parameter
+        learning_rate = params.learning_rate
+        loss_function = tf.losses.sparse_softmax_cross_entropy
+        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+
+        # === input data ===
+        user_features_all = tf.feature_column.input_layer(placeholders['u_features'],
+                                                          user_features_columns)
+        item_features_all = tf.feature_column.input_layer(placeholders['v_features'],
+                                                          item_features_columns)
+        user_features_all = tf.cast(user_features_all, tf.float64)
+        item_features_all = tf.cast(item_features_all, tf.float64)
+
+        # get batch
+        user_features_batch = tf.sparse.matmul(placeholders['user_id'], user_features_all)
+        item_features_batch = tf.sparse.matmul(placeholders['item_id'], item_features_all)
+
+        # === dense layers at the first level ===
+        f_user = tf.layers.dense(user_features_batch,
+                                 units=dim_user_raw,
+                                 activation=None,
+                                 kernel_initializer=tf.glorot_normal_initializer(),
+                                 kernel_regularizer=regularizer(regularizer_parameter),
+                                 use_bias=True,
+                                 name='user_features')
+        f_item = tf.layers.dense(item_features_batch,
+                                 units=dim_item_raw,
+                                 activation=None,
+                                 kernel_initializer=tf.glorot_normal_initializer(),
+                                 kernel_regularizer=regularizer(regularizer_parameter),
+                                 use_bias=True,
+                                 name='item_features')
+
+        # batch norm
+        f_user = tf.cast(f_user, tf.float32)
+        f_item = tf.cast(f_item, tf.float32)
+
+        f_user = tf.contrib.layers.batch_norm(f_user,
+                                              is_training=placeholders['training'],
+                                              trainable=True)
+        f_item = tf.contrib.layers.batch_norm(f_item,
+                                              is_training=placeholders['training'],
+                                              trainable=True)
+
+        f_user = tf.cast(f_user, tf.float64)
+        f_item = tf.cast(f_item, tf.float64)
+
+        # activiation
+        f_user = tf.nn.relu(f_user)
+        f_item = tf.nn.relu(f_item)
+
+        # dropout
+        f_user = tf.layers.dropout(f_user, rate=dropout, training=placeholders['training'])
+        f_item = tf.layers.dropout(f_item, rate=dropout, training=placeholders['training'])
+        h_user = tf.layers.dropout(h_user, rate=dropout, training=placeholders['training'])
+        h_item = tf.layers.dropout(h_item, rate=dropout, training=placeholders['training'])
+
+        # === dense layers at the 2nd level ===
+        f_user = tf.layers.dense(f_user,
+                                 units=dim_user_embedding,
+                                 activation=None,
+                                 kernel_initializer=tf.glorot_normal_initializer(),
+                                 kernel_regularizer=regularizer(regularizer_parameter),
+                                 use_bias=False,
+                                 name='f_user')
+        h_user = tf.layers.dense(h_user,
+                                 units=dim_user_embedding,
+                                 kernel_initializer=tf.glorot_normal_initializer(),
+                                 kernel_regularizer=regularizer(regularizer_parameter),
+                                 use_bias=False,
+                                 name='h_user')
+        f_item = tf.layers.dense(f_item,
+                                 units=dim_item_embedding,
+                                 activation=None,
+                                 kernel_initializer=tf.glorot_normal_initializer(),
+                                 kernel_regularizer=regularizer(regularizer_parameter),
+                                 use_bias=False,
+                                 name='f_item')
+        h_item = tf.layers.dense(h_item,
+                                 units=dim_item_embedding,
+                                 kernel_initializer=tf.glorot_normal_initializer(),
+                                 kernel_regularizer=regularizer(regularizer_parameter),
+                                 use_bias=False,
+                                 name='h_item')
+
+        user_embedding = tf.nn.relu(f_user + h_user)
+        item_embedding = tf.nn.relu(f_item + h_item)
+
+        # === decoder ===
+        weights_decoder = []
+        with tf.variable_scope('decoder'):
+            for i in range(num_basis):
+                weights = tf.get_variable(name='decoder' + str(i),
+                                          shape=[dim_user_embedding,
+                                                 dim_item_embedding],
+                                          dtype=tf.float64,
+                                          trainable=True,
+                                          regularizer=regularizer(regularizer_parameter),
+                                          initializer=tf.glorot_normal_initializer()
+                                          )
+                weights_decoder.append(weights)
+
+            weight_combination = tf.get_variable(name='weight_combination',
+                                                 shape=[5, num_basis],
+                                                 dtype=tf.float64,
+                                                 trainable=True,
+                                                 regularizer=None,
+                                                 initializer=tf.glorot_normal_initializer()
+                                                 )
+
+        logits = []
+        for row in range(classes):
+            kernel = 0
+            for k in range(num_basis):
+                kernel += weight_combination[row, k] * weights_decoder[k]
+
+            uQ = tf.matmul(user_embedding, kernel)
+            uQv = tf.reduce_sum(tf.multiply(uQ, item_embedding), axis=1)
+            logits.append(uQv)
+        logits = tf.stack(logits, axis=1)
+
+        # predicted labels
+        predicted_classes = tf.argmax(logits, 1)
+
+        # === performance measure ===
+        self.loss = loss_function(labels=placeholders['labels'], logits=logits)
+        self.accuracy = tf.contrib.metrics.accuracy(labels=placeholders['labels'], predictions=predicted_classes)
+        self.mse = tf.losses.mean_squared_error(labels=placeholders['labels'], predictions=predicted_classes,
+                                                reduction=tf.Reduction.SUM_OVER_BATCH_SIZE)
+        # summary
+        tf.summary.scalar('loss', self.loss)
+        tf.summary.scalar('accuracy', self.accuracy)
+        tf.summary.scalar('mse', self.mse)
+
+        # training
+        self.training_op = optimizer.minimize(self.loss, global_step=self.global_step)
+
+
+
 
 
 
